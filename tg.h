a3e5443411326f020cd25a2acc4d38d5772f978a
Kirk Roerig <mr.possoms@gmail.com>
2019-06-30 09:14:38 -0600

    Documentation for the tg.h file

--- tg.h
+++ tg.h
@@ -31,8 +31,19 @@ typedef struct {
 	int _living_count;
 } tg_particle_system_t;
 
+/**
+ * @brief      Returns a random float.
+ *
+ * @return     Random float in the range [-1, 1]
+ */
 float tg_randf() { return (random() % 2048) / 1024.f - 1.f; }
 
+/**
+ * @brief      Computes next particle system state (animates) from the previous
+ *             state.
+ *
+ * @param      sys   The particle system.
+ */
 void tg_update_particle_sys(tg_particle_system_t* sys)
 {
 	tg_particle_t* parts = sys->particles;
@@ -72,7 +83,18 @@ void tg_update_particle_sys(tg_particle_system_t* sys)
 	}
 }
 
-
+/**
+ * @brief      Passes the row and column of the character being rendered. If
+ *             particles exist in that location, a non 0 character will be
+ *             returned.
+ *
+ * @param      sys   The particle system.
+ * @param[in]  row   The row of the character the terminal is rendering.
+ * @param[in]  col   The col of the character the terminal is rendering.
+ *
+ * @return     character to be displayed if there are particles present, 0
+ *             otherwise.
+ */
 char tg_sample_particle_sys(tg_particle_system_t const* sys, int row, int col)
 {
 	char c = 0;
@@ -97,7 +119,12 @@ char tg_sample_particle_sys(tg_particle_system_t const* sys, int row, int col)
 	return c;
 }
 
-
+/**
+ * @brief      Spawns given particle in the particle system.
+ *
+ * @param      sys   The particle system.
+ * @param      p     Pointer to particle that will be spawned in the system.
+ */
 void tg_spawn_particle(tg_particle_system_t* sys, tg_particle_t* p)
 {
 	if (sys->_living_count >= sizeof(sys->particles) / sizeof(tg_particle_t)) { return; }
@@ -106,13 +133,24 @@ void tg_spawn_particle(tg_particle_system_t* sys, tg_particle_t* p)
 	sys->_living_count++;
 }
 
-
+/**
+ * @brief      Removes all living particles from the system.
+ *
+ * @param      sys   The particle system.
+ */
 void tg_clear_particles(tg_particle_system_t* sys)
 {
 	sys->_living_count = 0;
 }
 
-
+/**
+ * @brief      Sets the terminal up to render a game like yours!
+ *
+ * @param      old_settings  The old terminal settings. Keep these to restore
+ *                           the previous settings for your terminal.
+ *
+ * @return     0 on success
+ */
 int tg_game_settings(struct termios* old_settings)
 {
 	struct termios newt;
@@ -130,7 +168,13 @@ int tg_game_settings(struct termios* old_settings)
 	return 0;
 }
 
-
+/**
+ * @brief      Restores a previously saved terminal settings object.
+ *
+ * @param      old_settings  The old terminal settings.
+ *
+ * @return     0 on success
+ */
 int tg_restore_settings(struct termios* old_settings)
 {
 	tcsetattr(STDIN_FILENO, TCSANOW, old_settings);
@@ -140,6 +184,11 @@ int tg_restore_settings(struct termios* old_settings)
 }
 
 
+/**
+ * @brief      Get the current width in columns of the terminal
+ *
+ * @return     width in columns, -1 if the value cannot be retrieved
+ */
 int tg_term_width()
 {
 	char buf[2048];
@@ -151,7 +200,11 @@ int tg_term_width()
 	return -1;
 }
 
-
+/**
+ * @brief      Get the current height in rows of the terminal
+ *
+ * @return     height in rows, -1 if the value cannot be retrieved
+ */
 int tg_term_height()
 {
 	char buf[2048];
@@ -163,7 +216,13 @@ int tg_term_height()
 	return -1;
 }
 
-
+/**
+ * @brief      Returns a key pressed withing TG_TIMEOUT microseconds
+ *
+ * @param      key   The key character pressed
+ *
+ * @return     1 if a key is pressed, 0 otherwise.
+ */
 int tg_key_get(char* key)
 {
 	fd_set fds;
@@ -190,20 +249,32 @@ int tg_key_get(char* key)
 	return read(STDIN_FILENO, key, sizeof(char)) == sizeof(char);
 }
 
-
+/**
+ * Context for drawing a string in the game.
+ */
 typedef struct {
-	int row;
-	int col;
-	const char* fmt;
+	int row;         // row where the origin of the string should appear.
+	int col;         // col where the origin of the string should appear.
+	const char* fmt; // printf style format string
 	struct {
+		// centered indicates that the string will appear centered around the origin.
 		uint8_t centered : 1;
-	} mode;
+	} mode;          // defines the mode of the format string's origin
 
 	char   _buf[1024];
 	size_t _len;
 } tg_str_t;
 
-
+/**
+ * @brief      Samples a string in the game.
+ *
+ * @param[in]  row        The row
+ * @param[in]  col        The col
+ * @param      ctx        The string context
+ * @param[in]  <unnamed>  variadic input to context's format string
+ *
+ * @return     { description_of_the_return_value }
+ */
 int tg_str(int row, int col, tg_str_t* ctx, ...)
 {
 	va_list ap;
@@ -215,7 +286,7 @@ int tg_str(int row, int col, tg_str_t* ctx, ...)
 	ctx->_len = strlen(ctx->_buf);
 
 	if (row != ctx->row) { return -1; }
-	
+
 	int adj_col = ctx->col;
 	if (ctx->mode.centered) { adj_col -= ctx->_len >> 1; }
 	if (col >= adj_col && col < adj_col + ctx->_len)
@@ -227,7 +298,13 @@ int tg_str(int row, int col, tg_str_t* ctx, ...)
 	return -1;
 }
 
-
+/**
+ * @brief      Erases the last 'rows' number of lines from the terminal
+ * and moves the cursor back up that same number of rows. This is intended
+ * to get ready for drawing a new frame.
+ *
+ * @param[in]  rows  The rows to clear
+ */
 void tg_clear(int rows)
 {
 	static char move_up[16] = {};
@@ -235,20 +312,26 @@ void tg_clear(int rows)
 	fprintf(stderr, "%s", move_up);
 }
 
-
+/**
+ * @brief      tg_rasterize simply iterates over each row and column from 0 to
+ * 'rows' and 0 to 'cols' for each row-col pair. With each pair, the `sampler`
+ * function pointer is called. This function pointer is user provided. And allows
+ * the programmer's game to dictate what should be displayed in each row-col pair
+ * by returning a pointer to the appropriate character.
+ *
+ * @param[in]  rows     The number of rows that will be sampled
+ * @param[in]  cols     The number of cols that will be sampled
+ * @param      sampler  The sampler function pointer, whose purpose is described
+ *                      above
+ */
 void tg_rasterize(int rows, int cols, char* (*sampler)(int row, int col))
 {
-	// line
-	//fprintf(stderr, "\033[91m");
 	for (int r = 0; r < rows; ++r)
 	for (int c = 0; c < cols; ++c)
 	{
 		char* glyph = sampler(r, c);
 		fprintf(stderr, "%s", glyph);
 	} fputc('\n', stderr);
-
-	//fprintf(stderr, "\033[39m");
-
 }
 
 #endif
